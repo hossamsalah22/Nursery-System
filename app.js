9bd2b64edb890f3bbbc8f194c0e56a087f5fbb25
Hossam Salah <hossamsalahabbas@gmail.com>
2023-02-19 17:08:47 +0200

    Updated Error Messages

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ server.use((require, result, next) => {
 	if (true) {
 		next();
 	} else {
-		next(new Error("not authentication "));
+		next(new Error("User Not Authenticated"));
 	}
 });
 
 server.use((require, result, next) => {
-	result.status(404).json({ massage: "page not found" });
+	result.status(404).json({ massage: "Sorry, Page Not Found" });
 });
 
 server.use((error, require, result, next) => {
