7271f3a321b0e0fa4c155903a50d0ba097c805a2
Hossam Salah <hossamsalahabbas@gmail.com>
2023-02-19 19:26:32 +0200

    ...

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
 
-const server = express();
 const port = process.env.PORT || 8080;
+const server = express();
+server.listen(port, () => console.log(`listening on http://localhost:${port}`));
 
 server.use(cors());
 server.use(logger("dev"));
 
 server.use((require, result, next) => {
-	result.status(404).json({ massage: "Sorry, Page Not Found" });
+	result.status(404).json({ massage: "Not Found" });
 });
 
 server.use((error, require, result, next) => {
 	result.status(500).json({ massage: error + "" });
 });
-
-server.listen(port, () => console.log(`listening on http://localhost:${port}`));
