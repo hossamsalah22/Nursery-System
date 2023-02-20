8e9092eef007fb60534f81a78ffcee305e19e5c1
Hossam Salah <hossamsalahabbas@gmail.com>
2023-02-20 18:27:48 +0200

    Child Validation Added

--- Routes/childsRoute.js
+++ Routes/childsRoute.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const controller = require("./../Controller/childsController");
 const validation = require("./../Core/validations/validationMiddleWare");
-const postValidation = require("./../Core/validations/childsValidation/postValidation");
-const patchValidation = require("./../Core/validations/childsValidation/patchValidation");
+const childValidation = require("./../Validation/childsValidation");
 const router = express.Router();
 
 router
 	.route("/childs")
 	.get(controller.getAllChilds)
-	.post(postValidation, validation, controller.addChild)
-	.patch(patchValidation, validation, controller.updateChild)
-	.delete(controller.deleteChild);
-router.get("/childs/:id", controller.getChild);
+	.post(childValidation.postValidation, validation, controller.addChild)
+	.patch(childValidation.patchValidation, validation, controller.updateChild)
+	.delete(childValidation.deleteValidation, validation, controller.deleteChild);
+router.get("/childs/:id", childValidation.getChildValidation, validation, controller.getChild);
+
 module.exports = router;
